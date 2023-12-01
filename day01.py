df3b771a9962d58e9c0ebc787794cca54d226231
Conor Hoekstra <codereport@outlook.com>
2023-12-01 12:50:15 -0500

    :recycle: ruff + fixes

--- day01.py
+++ day01.py
@@ -2,10 +2,10 @@
 
 # Part A
 
-f = 'data/01.txt'
+f = "data/01.txt"
 
 def num(s: str) -> int:
-    n = ''.join(c for c in s if c.isdigit())
+    n = "".join(c for c in s if c.isdigit())
     return int(n[0] + n[-1])
 
 print(sum(num(l) for l in open(f).readlines()))
@@ -15,7 +15,7 @@ print(sum(num(l) for l in open(f).readlines()))
 def translate(s: str) -> str:
     nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     conv = ["o1e", "t2o", "t3e",   "f4r",  "f5e",  "s6x", "s7n",   "e8t",   "n9e" ]
-    
+
     for n, c in zip(nums, conv):
         while n in s:
             s = s.replace(n, c)
